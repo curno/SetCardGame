30a3dbf32eef0aca08f0f938201c3adcef07ffc7
Curno <lyf.curno@gmail.com>
2014-02-22 10:10:51 +0800

    Initialize project directory structure
    Signed-off-by: Curno <lyf.curno@gmail.com>

---
+++ project/SET/SET/Include/UI/SETDlg.h
@@ -0,0 +1,31 @@
+
+// SETDlg.h : header file
+//
+
+#pragma once
+
+
+// CSETDlg dialog
+class CSETDlg : public CDialogEx
+{
+// Construction
+public:
+	CSETDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SET_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
